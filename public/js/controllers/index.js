cc296c4e9fbbe82fa1313ae05d0dc9e13e7488e1
Tuomas Saranen <tuomas.saranen@gmail.com>
2014-03-10 12:41:19 +0200

    holvin lisäys

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('mean.system').controller('IndexController', ['$scope', 'Global', function ($scope, Global) {
-    $scope.global = Global;
+	$scope.global = Global;
 
 	$scope.alerts = [
 		{ type: 'warning', msg: 'This site is under development, use at your own risk.' }
